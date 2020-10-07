1ad7e5329d3c9e41a2e286df51a9d6f03cb3ca9e
AmmoMax <steelratmax@gmail.com>
2020-10-07 23:02:57 +0300

    added color parser

--- utils/color.c
+++ utils/color.c
@@ -30,6 +30,18 @@ int	ft_atoi(char *str)
 	return (res * sign);
 }
 
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (*s)
+			s++;
+		else
+			return (NULL);
+	}
+	return (char *)s;
+}
+
 /*
 *
 * Валидирует строку с кодом цвета в RGB
@@ -51,7 +63,6 @@ static int	color_validator(char *line)
 	i = -1;
 	num_cnt = 0;
 	tmp = line;
-	// flag_color = 0;
 	while(line[++i])
 	{
 		if ((line[i] == 'C' || line[i] == 'F') && line[i + 1] == ' ')
@@ -71,9 +82,61 @@ static int	color_validator(char *line)
 	return (num_cnt == 3 ? 0 : 1);
 }
 
-static int	color_parser(char *line, m_config **m_config)
+static void	color_writer(char *line, color **color)
 {
-	return (0);
+	size_t	i;
+
+	i = 0;
+	while (line[i])
+	{
+		if (ft_isdigit(line[i]) && (line[i - 1] == ' ' || line[i - 1] == ','))
+		{
+			if ((*color)->red == -1)
+				(*color)->red = ft_atoi(line + i);
+			else if ((*color)->green == -1)
+				(*color)->green = ft_atoi(line + i);
+			else if ((*color)->blue == -1)
+				(*color)->blue = ft_atoi(line + i);
+			i++;
+		}
+		else
+			i++;
+	}
+}
+
+/*
+*
+* Парсит валидную строку, сохраняет данные в структуру и 
+* адрес этой структуры записывает в общую структуру config
+* C 100,255,255
+*/
+static void	color_parser(char *line, m_config **config)
+{
+	color	floor;
+	color	*floor_p;
+	color	ceiling;
+	color	*ceiling_p;
+
+	ceiling.red = -1;
+	ceiling.green = -1;
+	ceiling.blue = -1;
+	floor.red = -1;
+	floor.green = -1;
+	floor.blue = -1;
+	ceiling_p = &ceiling;
+	floor_p = &floor;
+	if (ft_strchr(line, 'C'))
+	{
+		printf("Parse ceiling");
+		color_writer(line, &ceiling_p);
+		(*config)->ceiling = ceiling_p;
+	}
+	if (ft_strchr(line, 'F'))
+	{
+		printf("Parse ceiling");
+		color_writer(line, &floor_p);
+		(*config)->floor = floor_p;
+	}
 }
 
 int	color_handler(char *line, m_config **config)
@@ -84,11 +147,12 @@ int	color_handler(char *line, m_config **config)
 	color		*ceiling_p;
 
 	if (color_validator(line) == 0)
-		return(color_parser(line, config));
+	{
+		color_parser(line, config);
+		return (0);
+	}
 	else
 		return (1);
-
-
 }
 
 
@@ -103,15 +167,10 @@ int main()
 	valid = "C 100,255,255";
 	invalid = "F 122,31 ";
 	if (color_handler(valid, &config_p) == 0)
-	{
 		printf("Color param is valid!\n");
-		// printf("Color param = %s");
-	}
 	else
-	{
 		printf("Color param is INvalid!\n");
-	}
 
-	// printf("Ceiling color. Red: %d, Green: %d, Blue: %d", config.ceiling->red, config.ceiling->green, config.ceiling->blue);
+	printf("Ceiling color. Red: %d, Green: %d, Blue: %d", config.ceiling->red, config.ceiling->green, config.ceiling->blue);
 	return (0);
 }
