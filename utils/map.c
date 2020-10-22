24db31c4064f015031d4a5cba049f87de69c5e8a
AmmoMax <steelratmax@gmail.com>
2020-10-22 22:59:11 +0300

    added first version validator for map structure

--- utils/map.c
+++ utils/map.c
@@ -72,6 +72,11 @@ char	get_up_neighbour(char *str, int max_len)
 	return (*tmp);
 }
 
+/*
+* Нормализует карту, дополняя кажду строку пробелами до длины самой длинной строки.
+* Принимает указатель на карту и максимальную длину строки в карте.
+*
+*/
 void		normalize_map(char **map, int max_len)
 {
 	size_t	i;
@@ -124,7 +129,6 @@ char		**convert_map(t_list *head)
 		head = head->next;
 	}
 	ft_strcpy(map[i], head->content);
-	// map[i + 1] = NULL;
 	normalize_map(map, max_len);
 	return (map);
 }
@@ -134,7 +138,7 @@ char		**convert_map(t_list *head)
 *
 * ФУНКЦИЯ ТЕСТОВАЯ, НЕ ИСПОЛЬЗОВАТЬ НА ПРОДЕ.
 */
-void	print_map(char **map, int len)
+void	print_map(char **map)
 {
 	size_t	i;
 
@@ -175,7 +179,7 @@ static int	common_line_map_v(char *line)
 * Если это первая строка карты - она вся содержит 1.
 * Если это последняя строка карты - она вся содержит 1 и EOF.
 */
-static int map_validator(char *line, m_config **config)
+static int map_validator_old(char *line, m_config **config)
 {
 	
 	if (!((*config)->map)) // если это первая строка
@@ -188,7 +192,7 @@ static int map_validator(char *line, m_config **config)
 /*
 * Добавляет в конец односвязного списка, который представляет карту, новую строку.
 * Принимает строку и общий конфиг.
-*
+* TODO: Добавить проверку выделения памяти под структуру и очистку
 */
 static int map_parser(char *line, m_config **config)
 {
@@ -205,11 +209,11 @@ static int map_parser(char *line, m_config **config)
 * Обработчик строки предполагаемой карты из файла.
 * Принимает строку с картой и общий конфиг.
 */
-int map_handler(char *line, m_config **config, int flag_last_line)
+int map_handler_old(char *line, m_config **config, int flag_last_line)
 {
 	if (flag_last_line == 0)
 	{
-		if (map_validator(line, config) == 0)
+		if (map_validator_old(line, config) == 0)
 			return (map_parser(line, config));
 		else
 			return (1);
@@ -221,6 +225,104 @@ int map_handler(char *line, m_config **config, int flag_last_line)
 			return (1);
 }
 
+/*
+* Общий обработчик строки карты. В зависимости строки вызывает нужный валидатор.
+* Принимает указатель на строку и флаг линии - по нему опрделяется первая строка,
+* последняя и все остальные.
+* Возвращает код: 1 - ошибка, 0 - ОК
+*/
+int	map_handler(char *line, m_config **config)
+{
+	return (map_parser(line, config));
+}
+
+/*
+* Валидирует карту из двумерного массива.
+* Вызывает три отдельных валидатор - для первой строки, для последней
+* и всех остальных.
+* Принимает указатель на карту и длину карты.
+* Возвращает 1 если карта не валидна и 0 если валидна
+*/
+int	map_validator(char **map, int len_map)
+{
+	size_t	i;
+	int		len_str;
+
+	i = -1;
+	while (map[++i])
+	{
+		len_str = ft_strlen(map[i]);
+		if (i == 0)
+			if (!(validator_1_str(map[i]) == 0))
+				return (1);
+		else if (i == (len_map - 1))
+			if (!(validator_last_str(map[i]) == 0))
+				return (1);
+		else
+			if (!(validator_common_str(map[i], len_str) == 0))
+				return (1);
+	}
+	return (0);
+}
+
+/*
+* Валидирует первую строку карты.
+* Первая строка может состоять из любого количество 1 и пробелов
+* Принимает указатель на строку.
+* Возвращает 0 если строка валидна и 1 если нет.
+*/
+static int	validator_1_str(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == 1 || str[i] == ' ')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/*
+* Валидирует последнюю строку карты.
+* Последняя строка как и первая может состоять только из пробелов и 1, т.к.
+* если предыдущая строка валидна то все дырки закрыты единицами.
+*/
+static int	validator_last_str(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while(str[i])
+	{
+		if (str[i] == 1 || str[i] == ' ')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+
+/*
+* Валирует обычную строку карты.
+* 
+* TODO: Добавить проверку на наличие в карте > 1 игрока.
+*/
+static int	validator_common_str(char *str, int len)
+{
+	size_t	i;
+
+	i = 0;
+	while(str[i])
+	{
+		
+	}
+
+}
+
 int main()
 {
 	char 		*map_line;
@@ -230,28 +332,31 @@ int main()
 	char 		**map;
 	size_t		i = 0;
 	char		str;
+	int			len_map;
 
 	config_p = &config;
 	config.map = NULL;
 	fd = open("map.cub", O_RDONLY);
 	while (get_next_line(fd, &map_line))
 	{
-		printf("map_line = %s\n", map_line);
-		map_parser(map_line, &config_p);
+		// printf("map_line = %s\n", map_line);
+		// map_parser(map_line, &config_p);
+		map_handler(map_line, &config_p);
 	}
-	printf("last line = %s\n", map_line);
-	map_parser(map_line, &config_p);
+	// printf("last line = %s\n", map_line);
+	map_handler(map_line, &config_p);
 	close(fd);
-	printf("max len in map: %d\n", max_len_line(config.map));
-	print_map(convert_map(config.map), max_len_line(config.map));
-	// map = convert_map(config.map), max_len_line(config.map);
+	map = convert_map(config.map);
+	print_map(map);
+	len_map = ft_lstsize(config.map);
+	if (map_validator(map, len_map))
+		printf("Map is NOT VALID!");
+	else
+		printf("Map is VALID!");
+		
+
+	// TODO: написать функцию очистки памяти под список с картой
 
 	// str = get_up_neighbour(&map[0][28], max_len_line(config.map));
-	// while (map[0][i])
-	// {
-	// 	printf("%ld ::: %c\n", i, map[0][i]);
-	// 	i++;
-	// }
-
 	return (0);
 }
