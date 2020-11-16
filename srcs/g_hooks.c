ee9917eaa0bd251c813749a1d6826ae9df754a3d
AmmoMax <steelratmax@gmail.com>
2020-11-16 20:22:48 +0300

    fixed moving sprites when player rotating

--- srcs/g_hooks.c
+++ srcs/g_hooks.c
@@ -6,7 +6,7 @@
 /*   By: amayor <amayor@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 22:47:59 by amayor            #+#    #+#             */
-/*   Updated: 2020/11/13 23:40:22 by amayor           ###   ########.fr       */
+/*   Updated: 2020/11/15 21:40:50 by amayor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,8 @@ int			move_forward(int keycode, t_world **world)
 	}
 	else if (keycode == 65361)
 	{
-		(*world)->plr->dir += M_PI / 90;
+		// (*world)->plr->dir += M_PI / 90;
+		(*world)->plr->dir += 0.02;
 		// (*world)->plr->dir += 0.02;
 		if ((*world)->plr->dir > 2 * M_PI)
 			(*world)->plr->dir -= 2 * M_PI;
@@ -85,13 +86,29 @@ int			move_forward(int keycode, t_world **world)
 	}
 	else if (keycode == 65363)
 	{
-		(*world)->plr->dir -= M_PI / 90;
-		// (*world)->plr->dir -= 0.02;
+		// (*world)->plr->dir -= M_PI / 90;
+		(*world)->plr->dir -= 0.02;
 		if ((*world)->plr->dir > 2 * M_PI)
 			(*world)->plr->dir -= 2 * M_PI;
 		else if ((*world)->plr->dir < 0)
 			(*world)->plr->dir += 2 * M_PI;
 		draw_3d_map(*world);
+	}
+	else if (keycode == 97)
+	{
+		x = (*world)->plr->x + SPEED;
+		y = (*world)->plr->y;
+		check_location(world, x + 0.5, y);
+		draw_3d_map(*world);
+		mlx_put_image_to_window((*world)->win->mlx, (*world)->win->win, (*world)->win->img, START_X, START_Y);
+	}
+	else if (keycode == 100)
+	{
+		x = (*world)->plr->x - SPEED;
+		y = (*world)->plr->y;
+		check_location(world, x - 0.5, y);
+		draw_3d_map(*world);
+		mlx_put_image_to_window((*world)->win->mlx, (*world)->win->win, (*world)->win->img, START_X, START_Y);
 	}
 	return (0);
 }
